20647c2df2694656eb8dcf15ab15ab896c9b92f4
DESKTOP-5P0IV89 <DESKTOP-5P0IV89@emxple.com>
2022-12-06 16:21:52 +0800

    更新自動更新脚本

---
+++ install_damominer.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+export PATH="/root/dev/snarkos"
+export CONF="/etc/supervisor/conf.d/"
+echo "判断是否安装锄头"
+
+
+function version() {
+  VERSION=$(curl -k -sL https://proxy.jeongen.com/https://api.github.com/repos/damomine/aleominer/releases | jq -r ".[0].tag_name")
+  echo "VERSION=${VERSION}"
+}
+
+
+
+function Install() {
+   sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
+  if [ -f ${PATH}/damominer ]; then
+    echo "已经安装过锄头"
+  else
+    version
+    echo "没有安装锄头,开始下载安装"
+    mkdir -p ${PATH}
+    cd ${PATH}
+    if [ ! -f ${PATH}/damominer_${VERSION}.tar ]; then
+      wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/damomine/aleominer/releases/download/$VERSION/damominer_$VERSION.tar
+      tar -xvf damominer_${VERSION}.tar
+      chmod a+x ${PATH}/damominer
+    fi
+
+    if [ ! -f ${PATH}/run-damominer.sh ]; then
+      wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/wxshope/shell/raw/master/run-damominer.sh
+      chmod +x run-damominer.sh
+    fi
+
+    if [ ! -f ${CONF}/damominer.conf ]; then
+      wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/wxshope/shell/raw/master/damominer.conf
+      mv damominer.conf ${CONF}
+    fi
+
+    chmod a+x ${PATH}/damominer
+
+    read -p "请输入您的钱包地址 > " wallet
+    sleep 4
+
+    sed -i "s/aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/$wallet/g" ${PATH}/run-damominer.sh
+  fi
+}
+
+function UPdata() {
+  version
+  if [ ! -d ${PATH} ]; then
+    mkdir -pv ${PATH}
+  fi
+  cd ${PATH}
+  wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/damomine/aleominer/releases/download/$VERSION/damominer_$VERSION.tar
+  tar -xvf damominer_${VERSION}.tar
+  chmod a+x ${PATH}/damominer
+  supervisorctl restart damominer
+}
+
+if [ X$1 = "Xinstall" ]; then
+  echo "開始部署 damominer 程序"
+   Install
+   echo -e "部署完成"
+   exit  0
+elif [ X$1 = "Xupdata" ]; then
+  echo "開始更新 damominer 程序"
+  UPdata
+  echo -e "完成更新"
+  exit  0
+else
+  echo "操作指令不正確  推出"
+  echo "輸入正確的指令   update OR install"
+  exit  0
+fi
--- run-damominer.sh
+++ run-damominer.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 export RUST_BACKTRACE=full
 export RUST_LOG=info
 ip=$(hostname -I | awk '{print $1}')
 url="aleo1.damominer.hk:9090"
-Address="aleo1ysrrdxz0pr2674vw9z0fvk6v8hc95kkwj23fffun522e2zl5mqqsj7duv6"
+Address="aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 path="/root/dev/snarkos"
-pid=$(ps -ef | grep ${Address} | grep -v grep  | awk '{print $2}')
-if [ x"$pid" = "x" ] ;then
-  nohup  ${path}/damominer --address ${Address} --proxy ${url} --worker ${ip}  &
+pid=$(ps -ef | grep ${Address} | grep -v grep | awk '{print $2}')
+if [ x"$pid" = "x" ]; then
+  echo -e  "${datetime}  DamoMiner START  successfully!"
+  nohup ${path}/damominer --address ${Address} --proxy ${url} --worker ${ip} &
 else
-  echo "${datetime}  lotus RUNNING  successfully!"
+  echo  -e "${datetime}  DamoMiner RUNNING  successfully!"
 fi
 wait
