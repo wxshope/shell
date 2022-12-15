0ad8ad620f4cbfe8c2076ba76f5c4860fa09b179
tddey01 <46470630+tddey01@users.noreply.github.com>
2022-12-15 15:24:40 +0800

    Update install_damominer.sh

--- install_damominer.sh
+++ install_damominer.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
+
 export FILEPATH="/root/dev/snarkos"
 export CONF="/etc/supervisor/conf.d/"
 echo "判断是否安装锄头"
 
-
 function version() {
+    if [ ! -d ${FILEPATH} ]; then
+      mkdir -pv ${FILEPATH}
+    fi
   VERSION=$(curl -k -sL https://proxy.jeongen.com/https://api.github.com/repos/damomine/aleominer/releases | jq -r ".[0].tag_name")
   echo "VERSION=${VERSION}"
+  SHELL_VERSION=$(cat version.txt)
 }
 
-
-
 function Install() {
-   sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl supervisor  clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
+  sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl supervisor clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
   if [ -f ${FILEPATH}/damominer ]; then
     echo "已经安装过锄头"
   else
     version
     echo "没有安装锄头,开始下载安装"
-    mkdir -p ${FILEPATH}
-    cd ${FILEPATH}
+
     if [ ! -f ${FILEPATH}/damominer_${VERSION}.tar ]; then
       wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/damomine/aleominer/releases/download/$VERSION/damominer_linux_$VERSION.tar
       tar -xvf damominer_linux_${VERSION}.tar
       chmod a+x ${FILEPATH}/damominer
-      rm damominer_linux_${VERSION}.tar 
-      rm README.md 
+      rm damominer_linux_${VERSION}.tar
+      rm README.md
       rm md5res
       rm run_gpu.sh
     fi
@@ -51,33 +52,36 @@ function Install() {
 
 function UPdata() {
   version
-  if [ ! -d ${FILEPATH} ]; then
-    mkdir -pv ${FILEPATH}
+  SHELL_NEW_VERSION=$(echo ${VERSION} | awk -Fv '{print $2}')
+  if [[ ${SHELL_NEW_VERSION} != ${SHELL_VERSION} ]]; then
+    wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/damomine/aleominer/releases/download/$VERSION/damominer_linux_$VERSION.tar
+    tar -xvf damominer_linux_${VERSION}.tar
+    chmod a+x ${FILEPATH}/damominer
+    supervisorctl restart damominer
+    rm damominer_linux_${VERSION}.tar
+    rm README.md
+    rm md5res
+    rm run_gpu.sh
+    echo ${VERSION} > version.txt
+  else
+    echo -e "${INFO} 当前已是最新版本[ ${SHELL_NEW_VERSION} ]!"
   fi
-  cd ${FILEPATH}
-  wget --limit-rate=10M -4 --tries=6 -c --no-check-certificate https://proxy.jeongen.com/https://github.com/damomine/aleominer/releases/download/$VERSION/damominer_linux_$VERSION.tar
-  tar -xvf damominer_linux_${VERSION}.tar
-  chmod a+x ${FILEPATH}/damominer
-  supervisorctl restart damominer
-      rm damominer_linux_${VERSION}.tar 
-      rm README.md 
-      rm md5res
-      rm run_gpu.sh
+
 }
 
 if [ X$1 = "Xinstall" ]; then
   echo "開始部署 damominer 程序"
-   Install
-   systemctl restart supervisor
-   echo -e "部署完成"
-   exit  0
+  Install
+  systemctl restart supervisor
+  echo -e "部署完成"
+  exit 0
 elif [ X$1 = "Xupdate" ]; then
   echo "開始更新 damominer 程序"
   UPdata
   echo -e "完成更新"
-  exit  0
+  exit 0
 else
   echo "操作指令不正確  推出"
   echo "輸入正確的指令   update OR install"
-  exit  0
+  exit 0
 fi
