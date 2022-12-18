64d6fabd951278717f731c787899e7e20cec05bf
tddey01 <46470630+tddey01@users.noreply.github.com>
2022-12-18 23:34:47 +0800

    Update run-damominer.sh

--- run-damominer.sh
+++ run-damominer.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 export RUST_BACKTRACE=full
 export RUST_LOG=info
-ip=$(hostname -I | awk '{print $1}')
+ip=$(hostname -I | awk '{print $1}'|awk -F. '{print "worker--" $3"--"$4}')
 url="aleo1.damominer.hk:9090"
 Address="aleoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 path="/root/dev/snarkos"
